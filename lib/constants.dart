9cad2701754dee2d89f85d6677218d9f5058b1cd
piyush8007848309 <piyushgadge800@gmail.com>
2020-04-18 16:41:29 +0530

    First commit

---
+++ lib/constants.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+
+const kMessageTextFieldDecoration = InputDecoration(
+  contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+  hintText: 'Type your message here...',
+  border: OutlineInputBorder(
+      borderRadius: BorderRadius.all(Radius.circular(32)),
+  ),
+);
+
+const kMessageContainerDecoration = BoxDecoration(
+  border: Border(
+    top: BorderSide(
+      width: 1,
+      color: Colors.lightBlueAccent,
+    ),
+  ),
+);
+
+const kTextFieldDecoration = InputDecoration(
+  hintText: 'Enter a Value',
+  contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+  border: OutlineInputBorder(
+    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+  ),
+  enabledBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Colors.lightBlueAccent, width: 1.0),
+    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+  ),
+  focusedBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Colors.lightBlueAccent, width: 2.0),
+    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+  ),
+);
